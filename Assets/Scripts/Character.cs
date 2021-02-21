77e289a4bd2a2401b2a2ac2be420e985543f54c1
JeffAntonelli <antonellijeffrey@gmail.com>
2021-02-21 19:07:36 +0100

    avec les fichiers c' est mieux

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 5f;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    private Vector2 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+        rb.MovePosition(rb.position + dir * speed * Time.fixedDeltaTime);
+    }
+}
